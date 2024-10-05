d41e00e2c4d649c376cd04ff516e50f4b258a2ff
noisyboy22 <mayurab666@gmail.com>
2024-10-05 23:58:53 +0530

    Routes to add data is created in app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 import auth
+from form_db import insert_student
 
 app = Flask(__name__)
 app.secret_key = "secret"
@@ -35,6 +36,32 @@ def register():
     else:
         flash('Registration failed. Username might already exist.')
         return redirect(url_for('register_page'))
+    
+@app.route('/addData')
+def addData():
+    return render_template('addData.html')
+
+# Form page to enter student details
+@app.route('/addData', methods=['POST'])
+def student_form():
+    if request.method == 'POST':
+        try:
+            name = request.form['name']
+            age = request.form['age']
+            grade = request.form['grade']
+            bio = request.form['bio']
+
+            if not name or not age or not grade or not bio:
+                flash('Please fill out all required fields.', 'error')
+            else:
+                if insert_student(name, int(age), grade, bio):
+                    flash('Student record added successfully!', category='success')
+                else:
+                    flash('A student with this name already exists.', category='error')
+        except:
+            msg = "Error in the INSERT"
+            flash(msg, 'error')
+            return render_template('addData.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
