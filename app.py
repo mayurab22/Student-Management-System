61c97a900eb25a8f5ac9a8630f73bc5a6bd900c4
noisyboy22 <mayurab666@gmail.com>
2024-10-06 00:08:13 +0530

    Comments and edit,delete routes are added

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
+#Import  
 from flask import Flask, render_template, request, redirect, url_for, flash
 import auth
-from form_db import insert_student
+from form_db import insert_student,fetch_all_students,fetch_student_by_id,update_student,delete_student
 
+#Flask constructor
 app = Flask(__name__)
-app.secret_key = "secret"
 
+#Default Route or root to render the login page 
 @app.route('/')
 def index():
     return render_template('login.html')
 
+#Route to Home page
+@app.route('/home')
+def dashboard():
+    return render_template('home.html')
+
+#Route to Login page with Authentication
 @app.route('/login', methods=['POST'])
 def login():
     username = request.form['username']
@@ -21,10 +29,12 @@ def login():
         flash('Invalid username or password. Please try again.')
         return redirect(url_for('index'))
 
+#Route to render register page
 @app.route('/register')
 def register_page():
     return render_template('register.html')
 
+#Route to Register new user
 @app.route('/register', methods=['POST'])
 def register():
     username = request.form['username']
@@ -36,7 +46,8 @@ def register():
     else:
         flash('Registration failed. Username might already exist.')
         return redirect(url_for('register_page'))
-    
+
+#Route to render Add New data page
 @app.route('/addData')
 def addData():
     return render_template('addData.html')
@@ -63,5 +74,33 @@ def student_form():
             flash(msg, 'error')
             return render_template('addData.html')
 
+#Route to view all the Student Data
+@app.route('/view')
+def view():
+    students = fetch_all_students()  # Fetch data from the database
+    return render_template('view.html', students=students)
+
+#Route to edit the student data
+@app.route('/edit/<int:id>', methods=['GET', 'POST'])
+def edit_student(id):
+    student = fetch_student_by_id(id)
+    if request.method == 'POST':
+        name = request.form['name']
+        age = request.form['age']
+        grade = request.form['grade']
+        bio = request.form['bio']
+        update_student(id, name, age, grade, bio)
+        flash('Student record updated successfully!', 'success')
+        return redirect(url_for('view'))
+    return render_template('edit_student.html', student=student)
+
+#Route to delete the student data
+@app.route('/delete/<int:id>', methods=['POST'])
+def delete_student_route(id):
+    delete_student(id)
+    flash('Student record deleted successfully!', 'success')
+    return redirect(url_for('view'))
+
+#Flask Python application is started and debug support is enabled so as to track any error
 if __name__ == '__main__':
     app.run(debug=True)
